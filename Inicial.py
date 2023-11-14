b0f5a2aa1d59e9307f1fc784ce759b62d3622544
tovenduvidio <150455566+tovenduvidio@users.noreply.github.com>
2023-11-14 01:02:39 -0300

    Update Inicial.py

--- Inicial.py
+++ Inicial.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.set_page_config(layout='wide', page_title='Página')
+
+
+st.title('Olááááááá')
+st.write('Esse site foi feito para você ter várias coisitas pra fazer, são só vídeos mas pode ajudar a se divertir')
+
+st.caption('1 - A aba de :blue[Vídeos] tem alguns vídeos engraçados')
+st.caption('2 - A aba de :blue[Desenhos] tem alguns episódios de desenhos que você pode assistir (em breve serão adicionados mais)')
+st.caption('3 - A aba de :blue[Karaoke] tem  um sistema com várias músicas pra filtrar e o vídeo de karaoke, junto tem uma aba com a planilha para adicionar mais músicas')
